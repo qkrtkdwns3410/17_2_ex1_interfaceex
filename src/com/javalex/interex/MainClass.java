ee9ffd67e9a8d089b782d5a319a61df253f58599
psj <qkrtkdwns3410>
2021-05-11 20:13:27 +0900

    Initial commit

---
+++ src/com/javalex/interex/MainClass.java
@@ -0,0 +1,14 @@
+package com.javalex.interex;
+
+public class MainClass {
+	
+	public static void main(String[] args) {
+		InterfaceClass interfaceClass = new InterfaceClass();
+		interfaceClass.calculate();
+		interfaceClass.getStr();
+		
+		InterfaceEx ife = new InterfaceClass();
+		InterfaceEx ife2 = new InterfaceClass();
+	}
+	
+}
